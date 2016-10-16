c6bd1d020e02cd6e11d97fd1f8baf75f43a89513
Tessabbs <tabbottsbury@hotmail.co.uk>
2016-10-16 20:31:14 +0100

    slider not working

--- Javascript.js
+++ Javascript.js
@@ -1,73 +1,42 @@
-var ul;
-var li_items; 
-var li_number;
-var image_number = 0;
-var slider_width = 0;
-var image_width;
-var current = 0;
-function init(){	
-	ul = document.getElementById('image_slider');
-	li_items = ul.children;
-	li_number = li_items.length;
-	for (i = 0; i < li_number; i++){
-		// nodeType == 1 means the node is an element.
-		// in this way it's a cross-browser way.
-		//if (li_items[i].nodeType == 1){
-			//clietWidth and width???
-			image_width = li_items[i].childNodes[0].clientWidth;
-			slider_width += image_width;
-			image_number++;
-	}
-	
-	ul.style.width = parseInt(slider_width) + 'px';
-	slider(ul);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function slider(){		
-		animate({
-			delay:17,
-			duration: 3000,
-			delta:function(p){return Math.max(0, -1 + 2 * p)},
-			step:function(delta){
-					ul.style.left = '-' + parseInt(current * image_width + delta * image_width) + 'px';
-				},
-			callback:function(){
-				current++;
-				if(current < li_number-1){
-					slider();
-				}
-				else{
-					var left = (li_number - 1) * image_width;					
-					setTimeout(function(){goBack(left)},2000); 				
-					setTimeout(slider, 4000);
-				}
-			}
-		});
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-function goBack(left_limits){
-	current = 0;	
-	setInterval(function(){
-		if(left_limits >= 0){
-			ul.style.left = '-' + parseInt(left_limits) + 'px';
-			left_limits -= image_width / 10;
-		}	
-	}, 17);
-}
-function animate(opts){
-	var start = new Date;
-	var id = setInterval(function(){
-		var timePassed = new Date - start;
-		var progress = timePassed / opts.duration
-		if(progress > 1){
-			progress = 1;
-		}
-		var delta = opts.delta(progress);
-		opts.step(delta);
-		if (progress == 1){
-			clearInterval(id);
-			opts.callback();
-		}
-	}, opts.dalay || 17);
-}
-window.onload = init;
 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Automatic slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+   var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+   slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+   slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
