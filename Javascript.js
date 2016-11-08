31d0a8ec21fe30276782c9d31bd41928d985312c
Tessabbs <tabbottsbury@hotmail.co.uk>
2016-11-08 19:20:46 +0000

    dont know

--- Javascript.js
+++ Javascript.js
@@ -4,4 +4,5 @@ function buttonnamechange() {
     document.getElementById("buttonname").innerHTML="Thank you!";
 }
 
-
+function initMap() { var uluru = {lat: -30.363, lng: 191.044}; var map = new google.maps.Map(document.getElementById('map'), 
+{ zoom: 4, center: uluru }); var marker = new google.maps.Marker({ position: uluru, map: map }); }
